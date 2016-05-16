73ebde264523702c2dd93e34b618c2ec1cf1ad02
杨畅 <1013717388@qq.com>
2016-05-16 21:27:47 +0800

    yc

--- models/mail.js
+++ models/mail.js
@@ -2,22 +2,22 @@
 * @Author: Administrator
 * @Date:   2016-05-13 22:10:01
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-05-15 12:27:22
+* @Last Modified time: 2016-05-16 21:27:22
 */
 
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-    host: "smtp.163.com",
+    host: 
     port: 25,
     auth: {
-        user: 'wy_zhou@163.com',
-    	pass: '13959960865'
+        user: 
+    	pass: 
     }
 });
 
 var mailOptions = {
-    from: 'zhou <wy_zhou@163.com> ', // sender address
+    from: '', // sender address
     to: '1013717388@qq.com', // list of receivers
     subject: 'Hello ✔', // Subject line
     text: 'Hello world ✔', // plaintext body
